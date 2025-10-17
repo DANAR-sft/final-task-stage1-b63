2ab2068bc81d1b90e57154446f986b2696e7e565
DANAR-sft <mdanartss@gmail.com>
2025-10-17 12:55:46 +0700

    feat: update update and delete

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ hbs.registerHelper("splitLines", function (text) {
 });
 
 app.use('/assets', express.static('src/assets/'))
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(session({
   secret: 'kunciRahasia',
   resave: false,
@@ -66,8 +66,15 @@ app.get('/logout', logout);
 app.get('/home', home);
 app.get('/project', project);
 app.post('/project', upload.single('upload_img'), handleProject);
-app.get('/experiences', experiences)
-app.post('/experiences', upload.single('logo_perus'), handleExperiences)
+app.get('/experiences', experiences);
+app.post('/experiences', upload.single('logo_perus'), handleExperiences);
+app.get('/list', list);
+app.get('/editP/:id', editP);
+app.post('/editP/:id',upload.single('edit_img'), handleEditP)
+app.get('/editE/:id', editE);
+app.post('/editE/:id',upload.single('logo_perus'), handleEditE)
+app.get('/deleteP/:id', deletePard);
+app.get('/deleteE/:id', deleteEard);
 
 // halaman login
 function login(req, res) {
@@ -202,9 +209,151 @@ async function handleExperiences(req, res) {
   const insert = `INSERT INTO experiences(pekerjaan, perusahaan, logo_perusahaan, start_date, end_date, tech_stack, deskripsi) VALUES ($1, $2, $3, $4, $5, $6, $7)`;
   const resultI = await db.query(insert, [pekerjaan, perusahaan, req.file.filename, s_date, e_date, techFilter, desc_exp]); 
 
-  res.redirect('/home')
+  res.redirect('/home');
 
 };
+
+async function list(req, res) {
+
+  const selectE = `SELECT * FROM experiences ORDER BY id DESC`;
+  const resultE = await db.query(selectE);
+
+  const selectP = `SELECT * FROM projects ORDER BY id DESC`;
+  const resultP = await db.query(selectP);
+
+  let userSession;
+  let afterLogin;
+  let beforeLogin;
+
+  if(req.session.user) {
+
+    userSession = { 
+      name: req.session.user.name, 
+     };
+    afterLogin = 'Sign out';
+
+  } else {
+    
+    userSession = { name: 'Anonim' };
+    beforeLogin = 'Login';
+
+  };
+
+  res.render('list_index', { userSession, afterLogin, beforeLogin, resultP, resultE });
+  
+};
+
+
+async function editP(req, res) {
+
+  let { id } = req.params;
+
+  let userSession;
+  let afterLogin;
+  let beforeLogin;
+
+  if(req.session.user) {
+
+    userSession = { 
+      name: req.session.user.name, 
+     };
+    afterLogin = 'Sign out';
+
+  } else {
+    
+    userSession = { name: 'Anonim' };
+    beforeLogin = 'Login';
+
+  };
+
+  res.render('edit_project', { userSession, afterLogin, beforeLogin, id });
+
+}; 
+
+
+async function handleEditP(req, res) {
+
+  let { id } = req.params;
+
+  let { edit_name, edit_img, edit_tech, edit_desc } = req.body;
+
+  const techFilter = Array.isArray(edit_tech) ? edit_tech : [edit_tech];
+
+  const editProject = `UPDATE projects SET img_project = $1, project_name = $2, deskripsi = $3, tech_project = $4 WHERE id = '${id}'`;
+
+  const resultedit = await db.query(editProject, [req.file.filename, edit_name, edit_desc, techFilter ])
+
+  res.redirect('/list');
+};
+
+
+async function editE(req, res) {
+
+  let { id } = req.params;
+  
+
+  let userSession;
+  let afterLogin;
+  let beforeLogin;
+
+  if(req.session.user) {
+
+    userSession = { 
+      name: req.session.user.name, 
+     };
+    afterLogin = 'Sign out';
+
+  } else {
+    
+    userSession = { name: 'Anonim' };
+    beforeLogin = 'Login';
+
+  };
+
+  res.render('edit_exp', { userSession, afterLogin, beforeLogin, id });
+
+};
+
+async function handleEditE(req, res) {
+
+  let { id } = req.params;
+
+  console.log(id)
+
+  let { pekerjaan, perusahaan, logo_perus, s_date, e_date, techexp, desc_exp} = req.body;
+
+  const techFilter = Array.isArray(techexp) ? techexp : [techexp];
+
+  const editExp = `UPDATE experiences SET pekerjaan= $1, perusahaan= $2, logo_perusahaan= $3, start_date= $4, end_date= $5, tech_stack= $6, deskripsi= $7 WHERE id = '${id}'`;
+  const resulteditE = await db.query(editExp, [pekerjaan, perusahaan, req.file.filename, s_date, e_date, techFilter, desc_exp ]); 
+
+  res.redirect('/list');
+
+};
+
+async function deletePard(req, res) {
+  
+  let { id } = req.params;
+
+  const hapus = `DELETE FROM projects WHERE id='${id}'`;
+
+  const resultD = await db.query(hapus);
+
+  res.redirect('/list');
+
+}
+
+async function deleteEard(req, res) {
+  
+  let { id } = req.params;
+
+  const hapus = `DELETE FROM experiences WHERE id='${id}'`;
+
+  const resultD = await db.query(hapus);
+
+  res.redirect('/list');
+
+}
 
 async function keproject(req, res) {
 
